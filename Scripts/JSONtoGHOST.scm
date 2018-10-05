72d62d76965400e093715f04e2d38d3b867dd75f
Eskender Tamrat <eskendertamrat52@gmail.com>
2018-10-05 18:54:53 +0300

    scm script for converting timeline to GHOST

--- Scripts/JSONtoGHOST.scm
+++ Scripts/JSONtoGHOST.scm
@@ -1,43 +1,61 @@
-(use-modules (ice-9 rdelim))
-(use-modules (ice-9 textual-ports))
+(use-modules (ice-9 hash-table))
+(use-modules (json))
 
-;; TODO: HANDLING GESTURE AND EMOTION RULES
-;; TODO: TESTING
-;; TODO: RUNNING THIS ON LOOP FOR ALL TIMELINE STORIES 
-;; TODO: ADDING COMMENTS / MAKING IT MORE READABLE
+(define port (open-input-file "1.json"))
+(define output-port (open-file "1.ghost" "a"))
+(define hashy (json->scm port))
 
-(let ((input-port (open-input-file "1.json")) (output-port (open-file "1.ghost" "a")))
-	(define keyval "start here")
-	(define linenum 1)
-	(do ((line (read-line input-port) (read-line input-port))) ((eof-object? line))
-		(if (= (string-length line) 1)
-			(begin
-				(display "u: RULE" output-port) 
-				(display linenum output-port)
-				(set! linenum (1+ linenum))
-				(display "()\n" output-port)
-			)
-			(begin
-				(set! line (string-trim-right line #\,))
-				(set! keyval (string-split (string-trim-both line) #\:))
-				(if (equal? (car keyval) "\"start_time\"")
-					(begin
-						(display "^" output-port)
-						(display "start_timer" output-port)
-						(display (cdr keyval) output-port)
-						(display "\n" output-port)
-					)
-					(display "\n") ;; continue	
+(define linenum 1)
+(define emote '("worry" "happySurprise" "happy.001" "recoil" "happyDisgust" "happy" "surprisedSad" "surprised" "sad" "irritated" "happy.002" "fearSuprise" "fear" "engaged" "disgustSurprise" "disgust.Sad" "disgust" "contempt" "confused" "comprehending" "bored" "awe" "angrySad" "angryDisgust" "angry" "amused" "none"))
+(for-each (lambda(ele) (display "u: RULE" output-port) (display linenum output-port) (display "() " output-port) (set! linenum (1+ linenum)) 
+	(display ele)
+	(display (hash-count (const #t) ele))
+	(if (hash-get-handle ele "gesture")
+		(begin
+			(display "^gesture(" output-port)
+			(display (cdr(hash-get-handle ele "gesture")))
+			(display (cdr(hash-get-handle ele "gesture")) output-port)
+			(if (member (cdr(hash-get-handle ele "gesture")) emote)
+				;;emote(name_node, magnitude_node, duration_node, blend_node)
+				(begin
+					(display ", " output-port)
+					(display (cadr(hash-get-handle ele "magnitude")) output-port)
+					(display ", " output-port)
+					(display (cdr(hash-get-handle ele "duration")) output-port)
+					;;(display ", 1)" output-port)
 				)
-				(if (equal? (car keyval) "\"text\"")
-					(begin
-						(display (cdr keyval) output-port)
-						(display "\n" output-port)
-					)
-					(display "\n") ;; continue
-				)
+				;;gesture(name_node, speed_node, magnitude_node, repeat_node)
+				(begin
+					(display ", " output-port)
+					(display (cdr(hash-get-handle ele "speed")) output-port)
+					(display ", " output-port)
+					(display (cadr(hash-get-handle ele "magnitude")) output-port)
+				)					
 			)
+			;;blend or repeat value
+			(display ", 1)" output-port)
 		)
+		#t
 	)
-	(close input-port)
-	(close output-port))
+
+(hash-for-each (lambda (key value)
+	(begin
+		(if (equal? key "start_time")
+			(begin
+				(display "^start_timer(" output-port)
+				(display value output-port)
+				(display ") " output-port)
+			)
+			#t
+		)
+		(if (equal? key "text")
+			(begin
+				(display value output-port)
+				(display " " output-port)
+			)
+			#t
+		)
+	)) 
+	ele) (display "\n" output-port)) hashy)
+(close port)
+(close output-port)
